870f87cb678a8bdce05aeb96e741c758b46f865e
yonghyun <chlgn44@naver.com>
2019-07-01 17:47:13 +0900

    0701

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+	  <Header></Header>
+    <v-content>
+      <router-view/>
+    </v-content>
+	<Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import store from './store'
+import Header from './components/Header'
+import Footer from './components/Footer'
+export default {
+	name: 'App',
+	store,
+	data() {
+		return {
+			//
+		}
+	},
+	components : {
+		Header,
+		Footer
+	}
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-toolbar fixed="true">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title><i class="fab fa-earlybirds fa-2x">YONGHYUN</i></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+    name : 'Header',
+    components : {
+
+    }
+}
+</script>
